48f5e8fb03305e36b2829719aaa4cd7d6112b043
JACKA <jackarcisse@gmail.com>
2024-05-17 19:58:14 +0300

    complete assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
-import React from "react";
-import { name, city } from "../data/data.js";
+import React from 'react';
+import { name, city } from '../data/user'; // Importing name and city variables
 
-function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
-}
+const Home = () => {
+  return (
+    <div id="home">
+      <h1 style={{ color: "firebrick" }}>{name} is a Web Developer from {city}</h1>
+    </div>
+  );
+};
 
 export default Home;
+
